47359d4df7fab97a5567e732f4025884bbdce935
maheshvp <maheshvp.cs@gmail.com>
2014-12-11 22:34:10 +0530

    Premotion aspnet appharbor integration added

--- RESTFulWCFService/DataModel.cs
+++ RESTFulWCFService/DataModel.cs
@@ -11,22 +11,87 @@ namespace RESTFulWCFService
     [DataContract]
     public class Employee
     {
+        private int _EmpId;
+        private string _Name;
+        private int _Age;
+        private string _Designation;
+        private string _Location;
+
         [DataMember]
-        public string Name { get; set; }
+        public int EmpId
+        {
+            get
+            {
+                return _EmpId;
+            }
+            set
+            {
+                _EmpId = value;
+            }
+        }
+
+
         [DataMember]
-        public Int32 Age { get; set; }
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+
         [DataMember]
-        public Int64 Salary { get; set; }
+        public Int32 Age
+        {
+            get
+            {
+                return _Age;
+            }
+            set
+            {
+                _Age = value;
+            }
+        }
+
+
         [DataMember]
-        public string Designation { get; set; }
+        public string Designation
+        {
+            get
+            {
+                return _Designation;
+            }
+            set
+            {
+                _Designation = value;
+            }
+        }
+
+
+        [DataMember]
+        public string Location
+        {
+            get
+            {
+                return _Location;
+            }
+            set
+            {
+                _Location = value;
+            }
+        }
     }
 
 
     public static class EmployeeData
     {
+        public static string constring = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
         public static List<Employee> GetEmployee()
         {
-            string constring = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
             using (SqlConnection con = new SqlConnection(constring))
             {
                 using (SqlCommand cmd = new SqlCommand("select * from Employees", con))
@@ -39,10 +104,11 @@ namespace RESTFulWCFService
                         {
                             Employee emp = new Employee();
 
+                            emp.EmpId = Convert.ToInt32(sdr["EmpId"].ToString());
                             emp.Name = sdr["Name"].ToString();
                             emp.Age = Convert.ToInt32(sdr["Age"]);
-                            emp.Salary = Convert.ToInt64(sdr["Salary"]);
                             emp.Designation = sdr["Designation"].ToString();
+                            emp.Location = sdr["Location"].ToString();
 
                             employee.Add(emp);
                         }
@@ -53,12 +119,80 @@ namespace RESTFulWCFService
             }
         }
 
-        public static bool AddEmployee(Employee emp)
+        public static Employee GetEmployee(int EmpId)
         {
-            string constring = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
             using (SqlConnection con = new SqlConnection(constring))
             {
-                using (SqlCommand cmd = new SqlCommand("insert into Employees values ('" + emp.Name + "'," + emp.Age + "," + emp.Salary + ",'" + emp.Designation + "')", con))
+                using (SqlCommand cmd = new SqlCommand("select top 1 * from Employees where EmpId=" + EmpId, con))
+                {
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sdr.Read();
+                        Employee emp = new Employee();
+
+                        emp.Name = sdr["Name"].ToString();
+                        emp.Age = Convert.ToInt32(sdr["Age"]);
+                        emp.Designation = sdr["Designation"].ToString();
+                        emp.Location = sdr["Location"].ToString();
+
+                        con.Close();
+                        return emp;
+                    }
+                }
+            }
+        }
+
+        public static bool AddEmployee(Employee emp)
+        {
+
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("insert into Employees values ('" + emp.Name + "'," + emp.Age + "," + emp.Designation + ",'" + emp.Location + "')", con))
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        return false;
+                    }
+
+                }
+            }
+        }
+
+        public static bool UpdateEmployee(Employee emp)
+        {
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("update Employees set Name='" + emp.Name + "',Age=" + emp.Age + ",Designation='" + emp.Designation + "',Location='" + emp.Location + "' where EmpId=" + emp.EmpId, con))
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        return false;
+                    }
+
+                }
+            }
+        }
+
+        public static bool DeleteEmployee(int EmpId)
+        {
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("delete from Employees where EmpId=" + EmpId, con))
                 {
                     try
                     {
