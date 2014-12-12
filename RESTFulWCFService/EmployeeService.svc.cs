e2be6527845685089b4812ae74a318e889fe9609
maheshvp <maheshvp.cs@gmail.com>
2014-12-12 18:20:24 +0530

    Service methods added

--- RESTFulWCFService/EmployeeService.svc.cs
+++ RESTFulWCFService/EmployeeService.svc.cs
@@ -10,14 +10,29 @@ namespace RESTFulWCFService
 {   
     public class EmployeeService : IEmployeeService
     {
-        public List<Employee> GetEmployee()
+        public List<Employee> GetEmployees()
         {
-            return EmployeeData.GetEmployee();
+            return EmployeeData.GetEmployees();
+        }
+
+        public Employee GetEmployee(int EmpId)
+        {
+            return EmployeeData.GetEmployee(EmpId);
         }
 
         public bool AddEmployee(Employee emp)
         {
             return EmployeeData.AddEmployee(emp);
+        }
+
+        public bool UpdateEmployee(Employee emp)
+        {
+            return EmployeeData.UpdateEmployee(emp);
+        }
+
+        public bool DeleteEmployee(int EmpId)
+        {
+            return EmployeeData.DeleteEmployee(EmpId);
         }
     }
 }
